4849a67456e4949311f7084501f4bb41f0acfcf6
Norbert Schlaffke <norbert.schlaffke@student.cs.put.poznan.pl>
2018-01-17 20:44:18 +0100

    pom.xml corrected

--- src/main/java/put/ai/games/player/Player.java
+++ src/main/java/put/ai/games/player/Player.java
@@ -6,24 +6,22 @@ package put.ai.games.player;
 
 import java.util.List;
 import java.util.Random;
-
 import put.ai.games.game.Board;
 import put.ai.games.game.Move;
 import put.ai.games.game.Player;
 
-public class player extends Player
+public class Player extends put.ai.games.game.Player
 {
 
-    private Random random = new Random(0xdeadbeef);
+    private Random random=new Random(0xdeadbeef);
+
     @Override
-    public String getName()
-    {
+    public String getName() {
         return "Gracz Naiwny 84868";
     }
 
     @Override
-    public Move nextMove(Board b)
-    {
+    public Move nextMove(Board b) {
         List<Move> moves = b.getMovesFor(getColor());
         return moves.get(random.nextInt(moves.size()));
     }
